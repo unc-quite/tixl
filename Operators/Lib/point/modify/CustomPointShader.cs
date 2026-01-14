d603ed9aeb10275bd5fcfc8d68b9375aec58e0b0
unc_quite <unc.quite@gmail.com>
2026-01-15 02:36:51 +0400

    Add more options for CustomPointShader+ more stuff in example

--- Operators/Lib/point/modify/CustomPointShader.cs
+++ Operators/Lib/point/modify/CustomPointShader.cs
@@ -17,27 +17,45 @@ internal sealed class CustomPointShader : Instance<CustomPointShader>,ITransform
     [Output(Guid = "e0097148-4395-4441-83d2-c5cf5b76bb61")]
     public readonly TransformCallbackSlot<BufferWithViews> Output = new();
 
-    [Input(Guid = "e77660e2-0fd0-45ea-8e0b-c607a757bb49")]
-    public readonly InputSlot<BufferWithViews> Points = new();
+        [Input(Guid = "e77660e2-0fd0-45ea-8e0b-c607a757bb49")]
+        public readonly InputSlot<T3.Core.DataTypes.BufferWithViews> Points = new InputSlot<T3.Core.DataTypes.BufferWithViews>();
 
-    [Input(Guid = "e9712b03-e7aa-4fe5-b5cf-f2c5d0c0b0df")]
-    public readonly InputSlot<string> ShaderCode = new();
+        [Input(Guid = "e9712b03-e7aa-4fe5-b5cf-f2c5d0c0b0df")]
+        public readonly InputSlot<string> ShaderCode = new InputSlot<string>();
 
-    [Input(Guid = "01898885-4140-4435-bb44-a7a6f6f32657")]
-    public readonly InputSlot<Vector3> Center = new();
+        [Input(Guid = "01898885-4140-4435-bb44-a7a6f6f32657")]
+        public readonly InputSlot<System.Numerics.Vector3> Center = new InputSlot<System.Numerics.Vector3>();
 
-    [Input(Guid = "a5c7863e-9c26-4109-9851-3244086b0ccc")]
-    public readonly InputSlot<float> A = new();
+        [Input(Guid = "a5c7863e-9c26-4109-9851-3244086b0ccc")]
+        public readonly InputSlot<float> A = new InputSlot<float>();
 
-    [Input(Guid = "e5a7649f-684e-4938-8ae3-7289f5b9ff45")]
-    public readonly InputSlot<float> B = new();
+        [Input(Guid = "e5a7649f-684e-4938-8ae3-7289f5b9ff45")]
+        public readonly InputSlot<float> B = new InputSlot<float>();
 
-    [Input(Guid = "b909844f-cff7-4907-9bc8-e9c2281582bf")]
-    public readonly InputSlot<float> C = new();
+        [Input(Guid = "b909844f-cff7-4907-9bc8-e9c2281582bf")]
+        public readonly InputSlot<float> C = new InputSlot<float>();
 
-    [Input(Guid = "20226539-a481-4df6-8dc7-cc65de915ea9")]
-    public readonly InputSlot<float> D = new();
+        [Input(Guid = "20226539-a481-4df6-8dc7-cc65de915ea9")]
+        public readonly InputSlot<float> D = new InputSlot<float>();
 
-    [Input(Guid = "dfbb9327-6cd2-41d3-8b2b-0abd7716471b")]
-    public readonly InputSlot<bool> IgnoreTemplate = new();
+        [Input(Guid = "864dfe5a-14f2-490f-be9c-ee13ad58605c")]
+        public readonly InputSlot<string> AdditionalCode = new InputSlot<string>();
+
+        [Input(Guid = "dfbb9327-6cd2-41d3-8b2b-0abd7716471b")]
+        public readonly InputSlot<bool> IgnoreTemplate = new InputSlot<bool>();
+
+        [Input(Guid = "2a0cf45c-bcf5-4802-b32b-84cef0f1c129")]
+        public readonly InputSlot<int> Count = new InputSlot<int>();
+
+        [Input(Guid = "776b96c2-ec0d-4668-9112-39badbf5b619")]
+        public readonly InputSlot<T3.Core.DataTypes.Texture2D> Texture = new InputSlot<T3.Core.DataTypes.Texture2D>();
+
+        [Input(Guid = "d669b4e9-8a3e-493c-93d1-da0267b41b1b")]
+        public readonly MultiInputSlot<SharpDX.Direct3D11.ShaderResourceView> ShaderResources = new MultiInputSlot<SharpDX.Direct3D11.ShaderResourceView>();
+
+        [Input(Guid = "aa12212a-f1e4-4efb-b3b5-b6861f09f66b")]
+        public readonly MultiInputSlot<SharpDX.Direct3D11.Buffer> ConstantBuffers = new MultiInputSlot<SharpDX.Direct3D11.Buffer>();
+
+        [Output(Guid = "3b249524-d36c-4502-8de5-d519799041b8")]
+        public readonly Slot<string> ShaderCode_ = new Slot<string>();
 }
